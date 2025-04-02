5aae6333d3ec0f489b5457ce0b121a139d08df83
Amandeep Singh <153711495+AmanCodeCraft@users.noreply.github.com>
2025-04-02 20:29:05 +0530

    Update showData.php

--- pro/showData.php
+++ pro/showData.php
@@ -1,3 +1,29 @@
-<?php  
+<?php
 
-?>
+require 'connection.php';
+
+session_start();
+
+if (!($_SESSION['id'])) {
+    header('Location:loginForm.php');
+}
+
+$my_id=$_SESSION['id'];
+
+$sql = "SELECT * FROM ashu WHERE id='$my_id'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo  $row['id'] . "<br>";
+        echo  $row['name'] . "<br>";
+        echo  $row['mail_id'] . "<br>";
+        echo  $row['pass'] . "<br>";
+    }
+} else {
+    echo "No Records...!";
+}
+
+echo '<a href="logout.php">Logout</a>';
